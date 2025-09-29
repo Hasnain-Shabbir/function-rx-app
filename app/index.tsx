51668912e0248fa1edadb4b559a8e8f74ca41879
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2025-09-29 12:21:01 +0500

    chore: add the color into config file

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-2xl font-bold text-blue-500">
+      <Text className="text-2xl font-bold text-primary-500">
         Welcome to the Function RX App
       </Text>
     </View>
