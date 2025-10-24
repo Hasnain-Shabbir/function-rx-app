5ab59292f782cf50d2f8ee042e95a07be84d0f19
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2025-10-24 17:43:34 +0500

    update: complete the latest sequence card design

--- components/SequenceCard/SequenceCard.tsx
+++ components/SequenceCard/SequenceCard.tsx
@@ -1,6 +1,7 @@
-import { CheckmarkCircle } from "@/assets/icons";
+import { CheckmarkCircle, WorkoutStretching } from "@/assets/icons";
 import Calendar from "@/assets/icons/svg/Calendar";
 import InformationCircle from "@/assets/icons/svg/InformationCircle";
+import PlayIcon from "@/assets/icons/svg/PlayIcon";
 import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
 import { View } from "react-native";
@@ -25,10 +26,10 @@ const SequenceCard = ({
   return (
     <LinearGradient
       colors={["#5171B1", "#364B76"]}
-      className="rounded-3xl overflow-hidden p-5 shadow-lg"
+      className="rounded-3xl overflow-hidden p-4 gap-4 shadow-lg"
     >
       {/* Header */}
-      <View className="flex-row justify-between items-center mb-3">
+      <View className="flex-row justify-between items-center">
         <Typography
           variant="body1"
           fontWeight="semibold"
@@ -40,68 +41,65 @@ const SequenceCard = ({
         <Tag type="info" title={status} />
       </View>
 
-      {/* Sequence Name */}
-      <Typography
-        variant="body2"
-        fontWeight="medium"
-        className="text-white mb-3"
-      >
-        {sequenceName}
-      </Typography>
-
-      {/* Details Row */}
-      <View className="flex-row items-center mb-4">
-        <View className="flex-row items-center mr-4 opacity-70 gap-1">
-          <Calendar />
-          <Typography variant="caption" className="text-white">
-            {date}
-          </Typography>
-        </View>
-        <Typography variant="caption" className="text-white/70 mr-4">
-          •
+      {/* Sequence Name + Sequence Details*/}
+      <View>
+        <Typography
+          variant="body2"
+          fontWeight="medium"
+          className="text-white mb-1"
+        >
+          {sequenceName}
         </Typography>
-        <View className="flex-row items-center opacity-70 gap-1">
-          <CheckmarkCircle color="white" size={16} />
-          <Typography variant="caption" className="text-white">
-            {completionCount}
+
+        <View className="flex-row items-center">
+          <View className="flex-row items-center mr-4 opacity-70 gap-1">
+            <Calendar />
+            <Typography variant="caption" className="text-white">
+              {date}
+            </Typography>
+          </View>
+          <Typography variant="caption" className="text-white/70 mr-4">
+            •
           </Typography>
+          <View className="flex-row items-center opacity-70 gap-1">
+            <CheckmarkCircle color="white" size={16} />
+            <Typography variant="caption" className="text-white">
+              {completionCount}
+            </Typography>
+          </View>
+        </View>
+
+        {/* Progress Section */}
+
+        <View className="flex-row items-center gap-2 mt-3">
+          <View className="flex-1 bg-primary-800 rounded-lg overflow-hidden h-1">
+            <View
+              className="bg-white rounded-full h-2"
+              style={{ width: `${progressPercentage}%` }}
+            />
+          </View>
+          <View className="flex-row items-center">
+            <WorkoutStretching color="#fff" size={14} />
+            <Typography variant="caption" className="text-white ml-1">
+              {progress}/{totalProgress}
+            </Typography>
+          </View>
         </View>
       </View>
-
-      {/* Progress Section */}
-      <View className="mb-4">
-        <View className="bg-primary-700 rounded-full h-2 mb-2">
-          <View
-            className="bg-white rounded-full h-2"
-            style={{ width: `${progressPercentage}%` }}
-          />
-        </View>
-        <View className="flex-row items-center justify-end">
-          <Typography variant="caption" className="text-white mr-1">
-            🏃
-          </Typography>
-          <Typography
-            variant="caption"
-            fontWeight="medium"
-            className="text-white"
-          >
-            {progress}/{totalProgress}
-          </Typography>
-        </View>
-      </View>
-
       {/* Message */}
-      <View className="flex-row items-center mb-4">
-        <Typography variant="caption" className="text-primary-200 mr-2">
-          ℹ️
-        </Typography>
-        <Typography variant="caption" className="text-primary-200">
+      <View className="flex-row items-center gap-1">
+        <InformationCircle />
+        <Typography
+          variant="caption"
+          className="text-white"
+          fontWeight="medium"
+        >
           {message}
         </Typography>
       </View>
 
       {/* Start Button */}
-      <Button variant={"secondary"} leftIcon={<InformationCircle />}>
+      <Button variant={"secondary"} leftIcon={<PlayIcon />}>
         Start Sequence
       </Button>
     </LinearGradient>
